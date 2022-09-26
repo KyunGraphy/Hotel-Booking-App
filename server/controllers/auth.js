07980cbd1f5a7c00e2c0cde17759c22ca0c6d2b6
KyunGraphy <onhoan0@gmail.com>
2022-09-26 16:34:11 +0700

    Use JWT with Cookie in Node.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   try {
@@ -25,13 +26,24 @@ export const login = async (req, res, next) => {
     if (!user) return next(createError(404, "User not found!"));
 
     const isPasswordCorrect = await bcrypt.compare(
-      req.body.password, user.password
+      req.body.password,
+      user.password
     );
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username!"));
 
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({...otherDetails});
+    res
+      .cookie("access_token", token, {
+        httpOnly: true
+      })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err)
   }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import authRoute from './routes/auth.js';
 import usersRoute from './routes/users.js';
 import hotelsRoute from './routes/hotels.js';
 import roomsRoute from './routes/rooms.js';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 dotenv.config();
 
@@ -22,6 +24,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // middlewares
+app.use(cookieParser());
 app.use(express.json())
 
 app.use('/api/auth', authRoute);
