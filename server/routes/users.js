ef46eb41ea727fc2df91ba82ca5e06cde3e288e8
KyunGraphy <onhoan0@gmail.com>
2022-09-28 11:29:34 +0700

    JWT Role-Based Authorization Verification

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,36 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("You are logged in!");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello User... You are logged in and you can delete your account!");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello Admin... You are logged in and you can delete all accounts!");
+// });
 
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
+
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
+
 // GET
+router.get("/:id", verifyUser, getUser);
 
-router.get("/:id", getUser);
 // GET ALL
-
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
