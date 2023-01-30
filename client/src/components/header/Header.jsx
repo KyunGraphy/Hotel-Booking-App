d1049097bf92c2106837aea993fb2ee75c016ddb
KyunGraphy <onhoan0@gmail.com>
2023-01-30 22:53:32 +0700

    React Login Auth Context API

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -15,6 +15,7 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
@@ -34,6 +35,7 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
+  const {user} = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -89,7 +91,7 @@ const Header = ({ type }) => {
               Get rewarded for your travels – unlock instant savings of 10% or
               more with a free Kyunbooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
