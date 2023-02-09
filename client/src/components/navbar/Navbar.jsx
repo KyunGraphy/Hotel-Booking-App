42e1e542a54e46acdf13c241017d6f84e3de4520
KyunGraphy <onhoan0@gmail.com>
2023-02-09 14:09:33 +0700

    Login Logout Function

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,21 +1,45 @@
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 import "./navbar.css"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext"
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(AuthContext);
+  
+  const handleClickLoginBtn = () => {
+    navigate("/login");
+  };
+
+  const handleClickLogOutBtn = () => {
+    dispatch({
+      type: "LOGOUT",
+    });
+  };
 
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" style={{color: "inherit", textDecoration: "none"}}>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">Kyunbooking</span>
         </Link>
-        {user ? user.username : <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>}
+        {user ? (
+          <div style={{ display: "flex" }}>
+            <span>{user.username}</span>
+            <div className="logOutItem" onClick={handleClickLogOutBtn}>
+              <FontAwesomeIcon icon={faSignOutAlt} />
+            </div>
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton" onClick={handleClickLoginBtn}>Login</button>
+          </div>
+        )}
       </div>
     </div>
   )
